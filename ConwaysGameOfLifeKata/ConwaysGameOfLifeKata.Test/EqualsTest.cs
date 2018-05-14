27ca90d1e372787a728536e61ff76cbbd24e97ae
Saish Dharvotkar <saish.dharvotkar@myob.com>
2018-05-15 08:16:19 +1200

    Created a test case for equals test

---
+++ ConwaysGameOfLifeKata/ConwaysGameOfLifeKata.Test/EqualsTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using ConwaysGameOfLifeKata.Kata;
+using Xunit;
+
+namespace ConwaysGameOfLifeKata.Test
+{
+    public class EqualsTest
+    {
+        [Fact]
+        public void GivenObjectAAndObjectBAreEqualThenReturnAsTrue()
+        {
+            var cellA = new List<CellLocation>
+            {
+                new CellLocation(1, 1)
+            };
+ 
+            var cellB = new List<CellLocation>
+            {
+                new CellLocation(1,1)
+                
+            };
+            
+           
+            
+            Assert.Equal(cellA, cellB);
+        }
+    }
+}
